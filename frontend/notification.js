7fd40f9cedcf653f5727e6f66079afc929563184
manpreet209 <man59112222@gmail.com>
2023-11-22 16:57:32 +0530

    new commit

---
+++ frontend/notification.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+import './index.css';
+
+
+
+
+ 
+<div className="notification">
+                
+               
+                alert(5 + 6);
+                  
+              </div>
